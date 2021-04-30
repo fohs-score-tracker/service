83b2847aba1aeba4594983c84e1c71b51034271a
Ian Worley <Therealianw@protonmail.com>
2021-04-29 20:41:03 -0700

    see if port change does the fix

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("scoretracker:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("scoretracker:app", host="0.0.0.0", port=8080, reload=True)
