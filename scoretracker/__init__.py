27f31f20dd6cc364f0fc55385487b831933a9f21
Wesley Henson <whenson9086@student.fortosage.net>
2021-05-03 15:00:19 -0500

    add CORS and fix other stuff

--- scoretracker/__init__.py
+++ scoretracker/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import users
 from .db import Base, engine
@@ -13,3 +14,11 @@ app = FastAPI(title="ScoreTracker", version="0.0.0", openapi_tags=[
     },
 ])
 app.include_router(users.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
