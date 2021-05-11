7c3b6f471cfa596bdd9ef9827f19de0c7c23db03
Ian Worley <Therealianw@gmail.com>
2021-05-11 10:03:27 -0500

    added remove endpoint

--- scoretracker/teams.py
+++ scoretracker/teams.py
@@ -14,13 +14,13 @@ router = APIRouter(tags=["Teams"])
              response_description="New Team", summary="Create a new team")
 def new_team(data: schemas.TeamCreate, redis: Redis = Depends(get_redis)):
     team = schemas.Team(id=redis.incr("next_team_id"), **data.dict())
-    current_team = f"team:{team.id}"
+    prefix = f"team:{team.id}"
     redis.sadd("teams", team.id)
-    redis.set(current_team + ":name", team.name)
+    redis.set(prefix + ":name", team.name)
     if team.players:
-        redis.sadd(current_team + ":players", *team.players)
+        redis.sadd(prefix + ":players", *team.players)
     if team.coaches:
-        redis.sadd(current_team + ":coaches", *team.coaches)
+        redis.sadd(prefix + ":coaches", *team.coaches)
 
     return team
 
@@ -28,24 +28,27 @@ def new_team(data: schemas.TeamCreate, redis: Redis = Depends(get_redis)):
 @router.get("/teams/{team_id}/", response_model=schemas.Team,
             summary="Lookup by id", responses={404: {"description": "Team does not exist"}})
 def get_team(team_id: int, redis: Redis = Depends(get_redis)):
-    current_team = f"team:{team_id}"
+    prefix = f"team:{team_id}"
     if not redis.sismember("teams", team_id):
         raise HTTPException(404)
 
     return schemas.Team(
         id=team_id,
-        name=redis.get(f"{current_team}:name"),
-        players=redis.smembers(f"{current_team}:players"),
-        coaches=redis.smembers(f"{current_team}:coaches"),
+        name=redis.get(f"{prefix}:name"),
+        players=redis.smembers(f"{prefix}:players"),
+        coaches=redis.smembers(f"{prefix}:coaches"),
     )
 
 
 @router.delete("/teams/{team_id}", status_code=204, responses={404: {"description": "Team does not exist"},
-               204: {"description": "Team was successfully deleted"}}, summary="Delete a team with id")
-def delete_team(team_id: int, redis: Redis = Dep(get_redis)):
-    key = f"team:{team_id}"
+                                                               204: {"description": "Team was successfully deleted"}}, summary="Delete a team with id")
+def delete_team(team_id: int, redis: Redis = Depends(get_redis)):
+    prefix = f"team:{team_id}"
     if not redis.sismember("teams", team_id):
         raise HTTPException(404)
     else:
-        redis.delete(key)
+        redis.srem("teams", team_id)
+        redis.delete(prefix + ":name")
+        redis.delete(prefix + ":players")
+        redis.delete(prefix + ":coaches")
     return Response(status_code=204)
