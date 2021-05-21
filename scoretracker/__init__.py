b999a2ecc839d1227b7dbdc7f595d50467a29621
DaShaun <826271+dashaun@users.noreply.github.com>
2021-05-21 09:18:07 -0500

    Update __init__.py
    Update origins.

--- scoretracker/__init__.py
+++ scoretracker/__init__.py
@@ -17,10 +17,16 @@ app = FastAPI(
     ],
 )
 
+origins = [
+    "ttps://fohs-score-tracker.github.io",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
