9688fe92c094d1b89be14a9666843cb15a52d6d4
Ian Worley <44382773+IanWorley@users.noreply.github.com>
2022-01-04 21:28:37 -0800

    Tests now work

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from .test_users import create_user
 
 
 def test_list_empty(client: TestClient):
@@ -7,19 +8,32 @@ def test_list_empty(client: TestClient):
 
 
 def test_create_get_list(client: TestClient):
-    expected1 = {"name": "First Player", "id": 1, "shots": []}
-    expected2 = {"name": "Second Player", "id": 2, "shots": []}
+    create_user(client, "dan", "dan@example.com", "a")
+    create_user(client, "stan", "stan@example.com", "a")
+
+    expected1 = {
+        "name": "First Player",
+        "user": {"name": "dan", "id": 1, "email": "dan@example.com"},
+        "id": 1,
+        "shots": [],
+    }
+    expected2 = {
+        "name": "Second Player",
+        "user": {"name": "stan", "id": 2, "email": "stan@example.com"},
+        "id": 2,
+        "shots": [],
+    }
 
     # create
     response1 = client.post(
         "/players/new",
-        json={"name": "First Player"},
+        json={"name": "First Player", "user_id": 1},
     )
     assert response1.status_code == 201 and response1.json() == expected1
 
     response2 = client.post(
         "/players/new",
-        json={"name": "Second Player"},
+        json={"name": "Second Player", "user_id": 2},
     )
     assert response2.status_code == 201 and response2.json() == expected2
 
@@ -37,16 +51,22 @@ def test_create_get_list(client: TestClient):
 
 
 def test_edit_and_list(client: TestClient):
-    expected = {"name": "Edited Player", "id": 1, "shots": []}
+    expected = {
+        "name": "Edited Player",
+        "id": 1,
+        "user": {"name": "dan", "id": 1, "email": "dan@example.com"},
+        "shots": [],
+    }
+    create_user(client, "dan", "dan@example.com", "a")
 
     client.post(
         "/players/new",
-        json={"name": "Player"},
+        json={"name": "Player", "user_id": 1},
     ).raise_for_status()
 
     response = client.patch(
         "/players/1",
-        json={"name": "Edited Player"},
+        json={"name": "Edited Player", "user_id": 1},
     )
     assert response.status_code == 200 and response.json() == expected
 
@@ -54,28 +74,37 @@ def test_edit_and_list(client: TestClient):
 
 
 def test_remove_player(client: TestClient):
+    create_user(client, "dan", "dan@example.com", "a")
     client.post(
         "/players/new",
         json={
             "name": "Deleted Player",
+            "user_id": 1,
         },
     ).raise_for_status()
 
     client.post(
         "/players/new",
-        json={"name": "Player"},
+        json={"name": "Player", "user_id": 1},
     ).raise_for_status()
 
     assert client.delete("/players/1").status_code == 204
 
-    assert client.get("/players").json() == [{"name": "Player", "id": 2, "shots": []}]
+    assert client.get("/players").json() == [
+        {
+            "name": "Player",
+            "user": {"name": "dan", "id": 1, "email": "dan@example.com"},
+            "id": 2,
+            "shots": [],
+        }
+    ]
 
 
 def test_404(client: TestClient):
     for response in (
         client.get("/players/1"),
         client.delete("/players/1"),
-        client.patch("/players/1", json={"name": "a"}),
+        client.patch("/players/1", json={"name": "a", "user_id": 1}),
     ):
         assert (
             response.status_code == 404
